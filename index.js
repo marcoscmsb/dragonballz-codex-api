3b3e78d713259c3498a53c6c9b48212832a3fdd4
Marcos <marcoscmsb@outlook.com>
2021-12-04 13:15:37 -0400

    inicializando API

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express")
+
+const api = express() // Cria APi com
+api.use(express.json()) //Habilita o copor de requisições em Json
+const cors = require("cors")
+api.use(cors({    //Habilita o Cors (Cross Origin Resource Sharing)
+    oring: "*"
+}))
+
+
+
+const personagens = [
+    {
+        nome: "Goku",
+        imagem: "https://sm.ign.com/t/ign_br/screenshot/default/goku_an6e.h720.jpg"
+    },
+    {
+        nome: "Vegita",
+        imagem: "https://pm1.narvii.com/6323/552a11b9216f3ff3f35544fe22faac36247693af_hq.jpg"
+    }
+]
+
+
+
+api.get("/", (req, res) =>{
+    res.json( { mensagem: "Olá mundo" } )
+})
+
+api.get("/personagens", (req, res) => {
+    res.json(personagens)
+})
+
+api.post("/personagens", (req, res) => {
+    personagens.push(req.body)
+    res.json( { mensagem: "Personagens salvo com sucesso!" } )
+})
+
+
+
+api.listen(3001, () =>{
+    console.log("Api esta rodando")
+})
