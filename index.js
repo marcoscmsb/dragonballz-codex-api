28687ee5b83d28a569400980701124c6cef1d5d9
Marcos <marcoscmsb@outlook.com>
2021-12-11 18:22:31 -0400

    Adicionando banco de dados

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 const api = express() // Cria APi com
 api.use(express.json()) //Habilita o copor de requisições em Json
@@ -7,20 +8,21 @@ api.use(cors({    //Habilita o Cors (Cross Origin Resource Sharing)
     oring: "*"
 }))
 
+
+async function connetc(){
+    await mongoose.connect("mongodb+srv://marcos:cmsbcelio45205420@cluster0.gt6iz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    console.log("Banco conectado com sucesso")
+}
+connetc()
+
+const personagemSchema = new mongoose.Schema({
+    nome: String,
+    imagem: String
+})
+
+const personagemModel = mongoose.model("Personagem",personagemSchema)
+
 const PORT = process.env.PORT  || 3001
-
-
-
-const personagens = [
-    {
-        nome: "Goku",
-        imagem: "https://sm.ign.com/t/ign_br/screenshot/default/goku_an6e.h720.jpg"
-    },
-    {
-        nome: "Vegita",
-        imagem: "https://pm1.narvii.com/6323/552a11b9216f3ff3f35544fe22faac36247693af_hq.jpg"
-    }
-]
 
 
 
@@ -28,12 +30,13 @@ api.get("/", (req, res) =>{
     res.json( { mensagem: "Olá Mundo" } )
 })
 
-api.get("/personagens", (req, res) => {
-    res.json(personagens)
+api.get("/personagens", async (req, res) => {
+    const resultado = await personagemModel.find({})  //Buscar todos
+    res.json(resultado)
 })
 
-api.post("/personagens", (req, res) => {
-    personagens.push(req.body)
+api.post("/personagens", async (req, res) => {
+    const resultado = await personagemModel.create(req.body) // Cadastro de personagem
     res.json( { mensagem: "Personagens salvo com sucesso!" } )
 })
 
